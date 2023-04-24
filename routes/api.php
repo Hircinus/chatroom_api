4b08184e1af221283ee541a169e3229dc28bff52
Jess <j.alfahad@gmail.com>
2023-04-24 14:03:54 -0400

    update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\PageController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +18,17 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::post('/ext/setUser', [PageController::class, 'setUser']);
+Route::get('/ext/getUsers', [PageController::class, 'getUsers']);
+Route::get('/ext/getUser/{search}', [PageController::class, 'getUser']);
+
+Route::post('/ext/setMessage', [PageController::class, 'setItem']);
+Route::get('/ext/getMessages', [PageController::class, 'getMessages']);
+
+/*
+Route::get('/ext/getItem/{search}', [PageController::class, 'getItem']);
+Route::get('/ext/getItemsByCategory/{search}', [PageController::class, 'getItemsByCategory']);
+Route::get('/ext/getCategoryByItem/{search}', [PageController::class, 'getCategoryByItem']);
+*/
